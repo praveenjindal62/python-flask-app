df389a145ae5f5447538384e1d25a47b152e7d8c
Praveen Jindal <praveenjindal62@gmail.com>
2022-04-16 13:34:06 +0530

    Commit

--- vm-python-app/code/app.py
+++ vm-python-app/code/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 content = os.getenv('VMAPP_CONTENT', 'VMApp Default Content')
 if len(content) == 0:
     content = 'VMApp Default Content'
-    
+
 @app.route('/')
 def hello():
     return content
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port=80)
