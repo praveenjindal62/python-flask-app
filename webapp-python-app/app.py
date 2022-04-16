f108c48fd76108f92a8202b99d2b4ea371cae677
Praveen Jindal <praveenjindal62@gmail.com>
2022-04-16 12:19:49 +0530

    Third Commit

--- webapp-python-app/app.py
+++ webapp-python-app/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+@app.route('/test')
+def get_data():
+    return requests.get('http://localhost:5100').content
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
