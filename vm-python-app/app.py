9fdd414d6fa2459548e9e3e123b461e1b8194ad9
Praveen Jindal <praveenjindal62@gmail.com>
2022-04-16 12:31:58 +0530

    default commit

--- vm-python-app/app.py
+++ vm-python-app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
-
+content = os.getenv('VMAPP_CONTENT', 'VMApp Default Content')
 @app.route('/')
 def hello():
-    return 'This text is from VM app'
+    return content
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port="5100")
